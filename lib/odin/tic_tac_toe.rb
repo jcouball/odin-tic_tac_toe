e8353653c476f288c1226d0d52d258ce471105fa
James Couball <jcouball@yahoo.com>
2025-09-18 08:23:25 -0700

    Generated project

---
+++ lib/odin/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tic_tac_toe/version"
+
+module Odin
+  module TicTacToe
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/odin/tic_tac_toe_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Odin::TicTacToe do
+  it "has a version number" do
+    expect(Odin::TicTacToe::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
